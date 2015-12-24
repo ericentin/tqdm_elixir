eaee047e966187ecf58b33d29578b2eea8e2c0fd
Eric Entin <eric@fanduel.com>
2015-12-23 19:14:04 -0500

    mix.exs

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule Tqdm.Mixfile do
      elixir: "~> 1.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
+     deps: deps,
+     description: description,
+     package: package]
   end
 
   # Configuration for the OTP application
@@ -28,5 +30,20 @@ defmodule Tqdm.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     []
+  end
+
+  defp description do
+    "Add a progress bar to your enumerables (Lists, Maps, Streams, Ranges, etc.) in a second."
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README.md", "LICENSE"],
+      maintainers: ["Eric Entin"],
+      licenses: ["Apache 2.0"],
+      links: %{
+        "GitHub" => "https://github.com/antipax/tqdm_elixir"
+      }
+    ]
   end
 end
